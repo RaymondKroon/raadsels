ebf297dff7361a9e9101d3c3705614c7ede87658
Raymond Kroon <raymond@k3n.nl>
2015-01-20 00:32:46 +0100

    beetje aankutten  met wat wel en niet werkt om het sneller te maken

--- src/raadsels/bucket.clj
+++ src/raadsels/bucket.clj
@@ -27,28 +27,14 @@
           :when (not (contains? not-wanted-results new-state))]
         (Path.  new-state (conj moves move))))
   (get-volumes [this]
-    (frequencies (map :volume (vals buckets)))))
-
-(extend-type clojure.lang.PersistentVector
-  Pour
-  (make-moves [this not-wanted-results]
-    (mapcat #(make-moves % not-wanted-results) this))
-  (get-volumes [this]
-    (map get-volumes this)))
-
-(extend-type clojure.lang.LazySeq
-  Pour
-  (make-moves [this not-wanted-results]
-    (mapcat #(make-moves % not-wanted-results) this))
-  (get-volumes [this]
-    (map get-volumes this)))
+    (into [] (map :volume (vals buckets)))))
 
 (defn possible-moves [buckets]
-  (for [from (keys buckets)
-        to (keys buckets)
-        :when (not= from to)
-        :when (not= (:volume (to buckets)) (:capacity (to buckets)))]
-    (Move. from to )))
+  (into [] (for [from (keys buckets)
+          to (keys buckets)
+          :when (not= from to)
+          :when (not= (:volume (to buckets)) (:capacity (to buckets)))]
+      (Move. from to ))))
 
 (defn pour [state move]
   (let [from ((:from move) state)
@@ -58,25 +44,23 @@
         (assoc-in [(:to move) :volume] (+ (:volume to) pour)))
 ))
 
-(defn give-solution [paths target]
-  (for [path paths
-        :when (= (get-volumes path) target)]
-    path))
+(defn find-solution [paths targets]
+  (first (drop-while #(not (contains? targets (get-volumes %))) paths)))
 
 (defn solve
   ([startbuckets target]
-   (solve [(Path. startbuckets [])] (frequencies target) #{startbuckets}))
+   (solve [(Path. startbuckets [])] target #{startbuckets}))
 
-  ([paths target explored]
+  ([paths targets explored]
    ;;(println (count paths))
-   (if (contains? (set (map get-volumes paths)) target)
-     (give-solution paths target)
-     (let [newpaths (make-moves paths explored)
+   (if-let [solution ( find-solution paths targets)]
+     solution
+     (let [newpaths (mapcat #(make-moves % explored) paths)
            newstates (map :buckets newpaths)
            newexplored (conj explored newstates)]
-     (recur newpaths target newexplored))
+     (recur newpaths targets newexplored))
      ))
   )
 
-;;(solve {:a (Bucket. 12 12) :b (Bucket. 8 0) :c (Bucket. 5 0)} [6 6 0])
-(with-progress-reporting (bench (solve {:a (Bucket. 12 12) :b (Bucket. 8 0) :c (Bucket. 5 0)} [6 6 0]) :verbose))
+;;(time (solve {:a (Bucket. 12 12) :b (Bucket. 8 0) :c (Bucket. 5 0)}  #{[6 6 0] [6 0 6] [0 6 6]}))
+(with-progress-reporting (bench (solve {:a (Bucket. 12 12) :b (Bucket. 8 0) :c (Bucket. 5 0)} #{[6 6 0] [6 0 6] [0 6 6]}) :verbose))
